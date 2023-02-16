1b4fb2ad65bd7ddc9090a09a654e9826de36c972
Canvendish <38320793+Canvendish@users.noreply.github.com>
2023-02-16 11:09:20 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -2,5 +2,7 @@ package org.devops
 
 
 def build(buildType,buildShell){
-    
+    def buildTools=["mvn":"MVN","npm":"NPM"]
+    buildHome=tool buildTools[buildType]
+    sh "${buildHome}/bin/${buildType} ${buildShell}"
 }
